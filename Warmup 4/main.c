4158ffccbf0073e562cd789adc099f0f49404b7a
danisnowman <danisnowman@gmail.com>
2018-07-28 16:33:11 -0600

    Initial Commit

---
+++ Warmup 4/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Warmup 4
+//
+//  Created by Daniel Hernández on 7/28/18.
+//  Copyright © 2018 Daniel Hernández. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
